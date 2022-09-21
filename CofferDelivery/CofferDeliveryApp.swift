4cca50a0102b12901420130ed405285baf339ead
刘瑞 <lr90719@163.com>
2022-09-21 11:25:01 +0800

    Initial Commit

---
+++ CofferDelivery/CofferDeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  CofferDeliveryApp.swift
+//  CofferDelivery
+//
+//  Created by JerryLiu on 2022/9/21.
+//
+
+import SwiftUI
+
+@main
+struct CofferDeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
